0a545ec63c21d9509653b7b00dbee43f840b5df0
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-07-13 22:07:15 +0100

    Create steal.js

---
+++ commands/economy/steal.js
@@ -0,0 +1,18 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+
+
+module.exports ={
+  name: "steal",
+  description: "Tells you your bot balance!",
+  execute(message, args){
+
+
+
+
+
+  }
+
+
+
+}
