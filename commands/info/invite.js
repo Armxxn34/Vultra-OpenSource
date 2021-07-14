9a486a91e727297e1267f103c6eae8527d45e491
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-07-14 16:15:41 +0100

    Create invite.js

---
+++ commands/info/invite.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+module.exports = {
+  name: 'invite',
+  description: 'Invite link',
+  category: 'info',
+  execute(message, args) {
+    const botAdd = `[Thank you for adding me!](https://discord.com/api/oauth2/authorize?client_id=835894096501014588&permissions=0&scope=applications.commands%20bot
+)`
+    const embed = new Discord.MessageEmbed()
+  
+      .setTitle("Invite")
+      .setFooter('Thank you for using Vultra!', '')
+      .setTimestamp()
+      .setColor("RANDOM")
+      .setDescription(botAdd);
+    message.channel.send(embed);
+  },
+};
