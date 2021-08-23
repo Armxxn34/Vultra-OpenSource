9f34d013a48dce02e5ca4b6ea9225c0124c3100e
LieutenantLickMe <62953987+LieutenantLickMe@users.noreply.github.com>
2021-08-23 21:32:11 +0100

    Fixed the syntax.
    fix yo buttons so I can actually start coding

--- handlers/musicHandler.js
+++ handlers/musicHandler.js
@@ -1,121 +1,103 @@
-const { MessageEmbed } = require("discord.js"), queue = new Map(), ytdl = require('ytdl-core');
-const Discord = require('discord.js')
+const { Discord } = require("discord.js");
+const queue = new Map();
+const ytdl = require('ytdl-core');
+const serverQueueEmbed = new Discord.MessageEmbed().setTitle('Leaving Voice Channel').setDescription(`Hey there, looks like you have no more songs left in queue, I'll leave the Voice Channel to save resources`).setColor('RANDOM');
+const serverPlayingEmbed = new Discord.MessageEmbed().setTitle('<a:music:838002356435812403> Now Playing <a:music:838002356435812403>').setColor('RANDOM');
 
 async function play(client, message, seek) {
-  const serverQueue = await getQueue(message);
+    const serverQueue = await getQueue(message);
 
-  if (!serverQueue.queue[0]) {
-    var meessage = `Hey there, looks like you have no more songs left in queue, I'll leave the Voice Channel to save resources`
-    var color = 'RANDOM'
-    var embeeeeed = new Discord.MessageEmbed()
-    .setTitle('Leaving Voice Channel')
-    .setDescription(meessage)
-    .setColor(color) 
-    serverQueue.textChannel.send(embeeeeed);
-    serverQueue.connection.disconnect();
-    queue.delete(message.guild.id);
-    return;
-  };
+    if (!serverQueue.queue[0]) {
+        serverQueue.connection.disconnect();
+        queue.delete(message.guild.id);
+        return serverQueue.textChannel.send(serverQueueEmbed);
+    };
 
-  if (typeof seek != Number || seek > serverQueue.queue[0].videoDetails.lengthSeconds) {
-    seek=0;
-  };
-  serverQueue.seek = seek
-  var title = serverQueue.queue[0].videoDetails.title
-   const row = new MessageActionRow()
-	.addComponents(
-	new MessageButton()
-	.setCustomId('playpause')
-	.setLabel('Play/Pause')
-	.setStyle('primary'),
-  .setEmoji(':play_pause:'),
-new MessageButton()
-	.setCustomId('skip')
-	.setLabel('Skip')
-	.setStyle('primary'),
-  .setEmoji(':skip:'),
-			);
-var embeddddddd = new Discord.MessageEmbed()
-.setTitle('<a:music:838002356435812403> Now Playing <a:music:838002356435812403>')
-.setDescription(`Now Playing:
- ${title}`)
-.setColor('RANDOM')
-.setImage()
-  serverQueue.textChannel.send(embeddddddd)
+    if (typeof seek != Number || seek > serverQueue.queue[0].videoDetails.lengthSeconds) {
+        seek=0;
+    };
 
-  serverQueue.dispatcher = serverQueue.connection
-    .play(ytdl.downloadFromInfo(serverQueue.queue[0], {filter: 'audioonly', highWaterMark: 1<<15}), {seek: serverQueue.seek})
-    .on("finish", () => {
-      if (serverQueue.loop.enabled === true) {
-        if (serverQueue.loop.single === true || serverQueue.queue.length < 2) {
-          play(client, serverQueue.textChannel);
-          return;
-        } else {
-          serverQueue.queue.push(serverQueue.queue[0]);
-          serverQueue.queueauthor.push(serverQueue.queueauthor[0]);
-          serverQueue.queue.shift();
-          serverQueue.queueauthor.shift();
-          play(client, serverQueue.textChannel);
-          return;
+    serverQueue.seek = seek
+
+    const row = new Discord.MessageActionRow();
+    row.addComponents(
+        new Discord.MessageButton().setCustomId('playpause').setLabel('Play/Pause').setStyle('primary').setEmoji(':play_pause:'),
+        new Discord.MessageButton().setCustomId('skip').setLabel('Skip').setStyle('primary').setEmoji(':skip:')
+    );
+
+    serverPlayingEmbed.setDescription(`Now Playing: ${serverQueue.queue[0].videoDetails.title}`);
+    serverQueue.textChannel.send(serverPlayingEmbed);
+
+    serverQueue.dispatcher = serverQueue.connection.play(ytdl.downloadFromInfo(serverQueue.queue[0], {filter: 'audioonly', highWaterMark: 1<<15}), {seek: serverQueue.seek}).on("finish", () => {
+        if (serverQueue.loop.enabled === true) {
+            if (serverQueue.loop.single === true || serverQueue.queue.length < 2) {
+                return play(client, serverQueue.textChannel);
+            } else {
+                serverQueue.queue.push(serverQueue.queue[0]);
+                serverQueue.queueauthor.push(serverQueue.queueauthor[0]);
+                serverQueue.queue.shift();
+                serverQueue.queueauthor.shift();
+                return play(client, serverQueue.textChannel);
+            }
         }
-      }
-      if (serverQueue.autoplay === true && !serverQueue.queue[1]) {
-        async () => {
-          var info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${serverQueue.queue[0].related_videos[0].id}`);
-          serverQueue.queue.push(info);
-          serverQueue.queueauthor.push(serverQueue.queueauthor[0]);
-          serverQueue.queue.shift();
-          serverQueue.queueauthor.shift();
-          play(client, serverQueue.textChannel);
+
+        if (serverQueue.autoplay === true && !serverQueue.queue[1]) {
+            async () => {
+                const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${serverQueue.queue[0].related_videos[0].id}`);
+                serverQueue.queue.push(info);
+                serverQueue.queueauthor.push(serverQueue.queueauthor[0]);
+                serverQueue.queue.shift();
+                serverQueue.queueauthor.shift();
+                play(client, serverQueue.textChannel);
+            }
+            return;
         }
-        return;
-      }
-      serverQueue.queue.shift();
-      serverQueue.queueauthor.shift();
-      play(client, serverQueue.textChannel);
-    })
-    .on("error", error => {
-      console.error(error);
+
+        serverQueue.queue.shift();
+        serverQueue.queueauthor.shift();
+        play(client, serverQueue.textChannel);
+    }).on("error", error => {
+        console.error("Error: " + error);
     });
 }
 
 
 async function getQueue(message) {
-  let queueConstruct = {
-    playing: false,
-    textChannel: null,
-    voiceChannel: null,
-    connection: null,
-    queue: [],
-    queueauthor: [],
-    dispatcher: null,
-    seek: 0,
-    loop: {
-      enabled: false,
-      single: false
+    let queueConstruct = {
+        playing: false,
+        textChannel: null,
+        voiceChannel: null,
+        connection: null,
+        queue: [],
+        queueauthor: [],
+        dispatcher: null,
+        seek: 0,
+        loop: {
+            enabled: false,
+            single: false
+        },
+        autoplay: false
     },
-    autoplay: false
-  },
-  serverQueue = queue.get(message.guild.id); //hold yo horses I realized I messed up the code in np//
-  if (!serverQueue) {
-    queue.set(message.guild.id, queueConstruct);
-    serverQueue = queueConstruct;
-  }
+        serverQueue = queue.get(message.guild.id); //hold yo horses I realized I messed up the code in np//
+    if (!serverQueue) {
+        queue.set(message.guild.id, queueConstruct);
+        serverQueue = queueConstruct;
+    }
 
-  return serverQueue;
+    return serverQueue;
 }
 
 async function updateQueue(message, newState) {
-  queue.set(message.guild.id, newState)
+    queue.set(message.guild.id, newState)
 }
 
 async function deleteQueue(message) {
-  queue.delete(message.guild.id)
+    queue.delete(message.guild.id)
 }
 
 module.exports = {
-  play,
-  getQueue,
-  updateQueue,
-  deleteQueue
+    play,
+    getQueue,
+    updateQueue,
+    deleteQueue
 }
