f216f15b11b91705fd19367b728e4273897cf4eb
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-12-12 13:45:15 +0000

    Update angry.js

--- commands/fun/angry.js
+++ commands/fun/angry.js
@@ -1,19 +1,20 @@
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('angry')
-		.setDescription('Why you angry?'),
-        cooldowns : new Set(),
-	    cooldown : 5,
+		.setDescription('Why u angry?'),
+cooldowns : new Set(),
+	    cooldown : 3,
 	async execute(interaction) {
-const array = ["https://media.giphy.com/media/11tTNkNy1SdXGg/giphy.gif", "https://media.giphy.com/media/3o9bJX4O9ShW1L32eY/giphy.gif", "https://media.giphy.com/media/26uf1EUQzKKGcIhJS/giphy.gif", "https://media.giphy.com/media/90FH7I3McAQ7u/giphy.gif"] 
-const randomgif = Math.floor(Math.random() * array.length); 
-const embed = new MessageEmbed() 
-.setTitle(`${interaction.user.username} is angry!`) 
-.setImage(`${array[randomgif]}`) 
-.setColor('RANDOM') 
+
+  const array = ["https://media.giphy.com/media/11tTNkNy1SdXGg/giphy.gif", "https://media.giphy.com/media/3o9bJX4O9ShW1L32eY/giphy.gif", "https://media.giphy.com/media/26uf1EUQzKKGcIhJS/giphy.gif", "https://media.giphy.com/media/90FH7I3McAQ7u/giphy.gif"]
+        const randomgif = Math.floor(Math.random() * array.length);
+const embed = new MessageEmbed()
+.setTitle(`${interaction.user.username} is angry!`)
+.setImage(`${array[randomgif]}`)
+.setColor('RANDOM')
 await interaction.reply({embeds: [embed]})
 	},
 };
